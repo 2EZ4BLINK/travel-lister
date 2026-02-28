94e4fecece578cf69fb8fb21b7bbe16450a5aa10
2EZ4BLINK <blinkcode24@gmail.com>
2026-02-28 21:17:13 +0800

    update: added a marker for each city i have

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 
 import { useGetLatAndLng } from "../hooks/useGetLatAndLng";
 import styles from "./Map.module.css";
+import { useCities } from "../contexts/CitiesContext";
 
 const Map = () => {
   const navigate = useNavigate();
+  const { cities } = useCities();
   const { lat, lng, setSearchParams } = useGetLatAndLng();
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
@@ -33,11 +35,23 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {(cities || []).map((city) => {
+          const {
+            position: { lat, lng },
+            id,
+            emoji,
+            cityName,
+          } = city;
+
+          return (
+            <Marker position={[lat, lng]} key={id}>
+              <Popup>
+                <span>{emoji}</span>
+                <span>{cityName}</span>
+              </Popup>
+            </Marker>
+          );
+        })}
       </MapContainer>
     </div>
   );
